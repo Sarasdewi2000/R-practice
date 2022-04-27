7b313058dc0b14be519e503cc002af0e183303db
Saraswati Saraswati <94231953+Sarasdewi2000@users.noreply.github.com>
2022-04-27 14:15:09 +0700

    Update first.R

--- first.R
+++ first.R
@@ -56,3 +56,20 @@ profil <- c(nama = "Budi", tempat_tinggal = "Jakarta", tingkat_pendidikan = "S1"
 
 print(profil) #Tampilkan variable profil
 
+#variable
+# List disimpan dalam variable dengan nama list_random
+list_random <- list(2, "Budi", 4)
+
+# Menampilkan isi list
+list_random 
+
+# List disimpan dalam variable dengan nama dati2
+dati2 <- list(nama = "Denpasar", propinsi = "Bali")
+
+# Menampilkan isi list dati2
+dati2 
+
+kota <- list(nama_kota = "Makassar", propinsi = "Sulawesi Selatan", luas_km2 = 199.3) # Buat variable kota sesuai permintaan soal
+
+# Tampilkan isi variable list kota
+kota
