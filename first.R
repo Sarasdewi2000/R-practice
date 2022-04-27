c626bda96475656a7544a91171fe20687ef43415
Saraswati Saraswati <94231953+Sarasdewi2000@users.noreply.github.com>
2022-04-27 13:43:09 +0700

    Update first.R

--- first.R
+++ first.R
@@ -4,6 +4,10 @@
 #simplecalculation
 10+20
 
+#print
+print ("Hello World")
+print (10+20)
+
 #variable
 a = 5 
 print (a)
@@ -14,3 +18,24 @@ c(1:5)
 
 #summary
 summary(c(1:5))
+
+#variable
+budi_berat_kg <- 68
+santi_berat_kg <- 54.5
+budi_berat_kg
+santi_berat_kg
+pi<- 3.14
+pi
+
+#vector
+c(1,2,3)
+ayam <- c(1,2,3)
+print(ayam)
+
+nama_mahasiswa <- c("Amira", "Budi", "Charlie")
+print (nama_mahasiswa)
+
+c(1:10)
+bebek <- c(1:10)
+print(bebek[5]))
+print(bebek[[1:5]])
