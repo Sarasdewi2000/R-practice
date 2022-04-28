9df867a17950e99635f10ae8c98f8846f7e1480d
Saraswati Saraswati <94231953+Sarasdewi2000@users.noreply.github.com>
2022-04-28 18:26:31 +0700

    Create library.R

---
+++ library.R
@@ -0,0 +1,47 @@
+#Menggunakan package ggplot2
+library(ggplot2)
+#Menggunakan package openxlsx
+library(openxlsx)
+
+#Membaca file mahasiswa.xlsx
+mahasiswa <- read.xlsx("https://storage.googleapis.com/dqlab-dataset/mahasiswa.xlsx",sheet = "Sheet 1")
+
+#Menampilkan data
+print(mahasiswa)
+
+#Menampilkan kolom Prodi
+print(mahasiswa$Prodi)
+
+library(ggplot2)
+#Menggunakan package openxlsx
+library(openxlsx)
+
+#Membaca file mahasiswa.xlsx
+mahasiswa <- read.xlsx("https://storage.googleapis.com/dqlab-dataset/mahasiswa.xlsx",sheet = "Sheet 1")
+
+#Membuat kanvas
+gambar <- ggplot(mahasiswa, aes(x=Fakultas, y=JUMLAH, fill=Fakultas))
+
+#Menambahkan objek bar chart, simpan kembali sebagai variable gambar
+gambar <- gambar + geom_bar(width=1, stat="identity")
+
+#Menggambar grafik
+gambar
+
+library(ggplot2)
+#Menggunakan package openxlsx
+library(openxlsx)
+
+#Membaca file mahasiswa.xlsx
+mahasiswa <- read.xlsx("https://storage.googleapis.com/dqlab-dataset/mahasiswa.xlsx",sheet = "Sheet 1")
+
+#Menghitung Jumlah Data by Fakultas
+summarybyfakultas <- aggregate(x=mahasiswa$JUMLAH, by=list(Kategori=mahasiswa$Fakultas, Tahun=mahasiswa$ANGKATAN), FUN=sum)
+summarybyfakultas <- setNames(summarybyfakultas, c("fakultas","tahun", "jumlah_mahasiswa"))
+summarybyfakultas
+
+summarybyfakultas$tahun = as.factor(summarybyfakultas$tahun)
+
+ggplot(summarybyfakultas, aes(x=fakultas, y=jumlah_mahasiswa)) + 
+  geom_bar(stat = "identity", aes(fill = tahun), width=0.8, position = position_dodge(width=0.8)) + 
+  theme_classic() 
