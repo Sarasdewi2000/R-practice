df138b03560c3d1ec4995677608827b8683e1f83
Saraswati Saraswati <94231953+Sarasdewi2000@users.noreply.github.com>
2022-05-07 18:34:15 +0700

    Create test.R

---
+++ test.R
@@ -0,0 +1,13 @@
+> y<- matrix(1:12,
++            2,
++            6,
++            )
+> print(y)
+
+first <- c("saras"); last <- c("wati")
+> first
+[1] "saras"
+> last
+[1] "wati"
+> paste(first,last)
+[1] "saras wati"
